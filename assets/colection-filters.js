ba167524c4fe98dd58d1899d6f6e9e229d7ef056
Roman <romazelenskiy2000@gmail.com>
2024-04-06 16:08:59 +0300

    fix arrow sorting

--- assets/colection-filters.js
+++ assets/colection-filters.js
@@ -7,54 +7,28 @@ filtersButton.addEventListener('click', toggleFilters);
 sortingButton.addEventListener('click', toggleSorting);
 
 async function toggleFilters() {
-  const isSortingOpen = sortingCollapse.classList.contains('active')
-  if (isSortingOpen) {
-    sortingCollapse.classList.add('no_animate')
-    sortingCollapse.classList.remove('active')
-  }
-  setTimeout(() => {
-    sortingCollapse.classList.remove('no_animate')
-    filterCollapse.classList.toggle('active')
-  }, 1)
+    const isSortingOpen = sortingCollapse.classList.contains('active')
+    if(isSortingOpen) {
+        sortingCollapse.classList.add('no_animate')
+        sortingCollapse.classList.remove('active')
+    }
+    setTimeout(() => {
+        sortingCollapse.classList.remove('no_animate')
+        filterCollapse.classList.toggle('active')
+    }, 1)
 }
 
 async function toggleSorting() {
-  const isFiltersOpen = filterCollapse.classList.contains('active')
-
-  if (isFiltersOpen) {
-    filterCollapse.classList.add('no_animate')
-    filterCollapse.classList.remove('active')
-  }
-  await setTimeout(() => {
-    filterCollapse.classList.remove('no_animate')
-    sortingCollapse.classList.toggle('active')
-    // sortingButton.classList.toggle('active')
-  }, 1)
-}
-
-
-// visibility button clear filters
-document.addEventListener('DOMContentLoaded', function () {
-  var clearFiltersButton = document.getElementById('clearFiltersButton');
-  function updateClearFiltersButton() {
-    var hasActiveFilters = false;
-    var filterCheckboxes = document.querySelectorAll('.checkbox_filters');
-    filterCheckboxes.forEach(function (checkbox) {
-      if (checkbox.checked) {
-        hasActiveFilters = true;
-      }
-    });
-    if (hasActiveFilters) {
-      clearFiltersButton.style.display = 'block';
-    } else {
-      clearFiltersButton.style.display = 'none';
+    const isFiltersOpen = filterCollapse.classList.contains('active')
+    if(isFiltersOpen) {
+        filterCollapse.classList.add('no_animate')
+        filterCollapse.classList.remove('active')
+        sortingButton.classList.remove('active')
     }
-  }
 
-  var filterCheckboxes = document.querySelectorAll('.checkbox_filters');
-  filterCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', updateClearFiltersButton);
-  });
-  updateClearFiltersButton();
-});
-//end
+    await setTimeout( () => {
+        filterCollapse.classList.remove('no_animate')
+        sortingCollapse.classList.toggle('active')
+        sortingButton.classList.toggle('active')
+    }, 1)
+}
